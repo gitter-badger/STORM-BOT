658f45c6061aefebd619b33fe7ebf9fe9f526de7
alosh55 <alosh.othman55@gmail.com>
2017-04-24 16:35:07 +0300

    Performance improvements

--- STORM.py
+++ STORM.py
@@ -1,48 +1,60 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-#  version 5.2.3
+
+#  version 5.2.4(326)
 #  DEV BY ALI .B .OTH
 
 import Kernal
-import multiprocessing
+from multiprocessing import cpu_count
 import platform
-import time
+from time import sleep
 
-if __name__ == "__main__":
+def compinfo():
+    np = cpu_count()
+    print '\nYou have {0:0} CPUs'.format(np)
 
-      try:
-            np = multiprocessing.cpu_count()
-            print 'You have {0:0} CPUs'.format(np)
-            
-            print
-            print 'system   :', platform.system()
-            print 'node     :', platform.node()
-            print 'release  :', platform.release()
-            print 'version  :', platform.version()
-            print 'machine  :', platform.machine()
-            print 'processor:', platform.processor()
-            
-            time.sleep(1)
-            print '\nSTARTING ',
-            
-            for i in range(1, 6) :
-                  time.sleep(1)
-                  print '.', 
-            if platform.python_version() < '2.7' or platform.python_version() >= '3.0':
-                  print'\nYour python version is ', platform.python_version()
-                  print '\nPlease install python 2.7'
-                  print '\nEXITING ',
-                  for i in range(1, 11) :
-                        time.sleep(1)
-                        print '.',                  
-            else:
-                  Kernal.start()
+    print
+    print 'system   :', platform.system()
+    print 'node     :', platform.node()
+    print 'release  :', platform.release()
+    print 'version  :', platform.version()
+    print 'machine  :', platform.machine()
+    print 'processor:', platform.processor()
 
-      except KeyboardInterrupt:
-            print '\nKeyboard INTERUPT (Ctrl+C)\nFIX ERROR AND TRY AGIN ! '
-      except:
-            print '\n\nERROR !!\nDISCONNECTED'
-            if platform.system() != 'Windows' :
-                  print '\nNot tested on ', platform.system()
-            print '\nPlease feedback: https://github.com/alosh55/STORM-BOT'
-            time.sleep(100000)
+    
+    
+def ver_py():
+    if platform.python_version() < '2.7' or platform.python_version() >= '3.0':
+        print'\nYour python version is ', platform.python_version()
+        print '\nPlease install python 2.7'
+        print '\nEXITING ',
+        for i in range(1, 11) :
+            sleep(1)
+            print '.',
+    else:
+        Kernal.start()            
+            
+def starting():
+    print '\nSTARTING ',
+    for i in range(1, 6) :
+        sleep(1)
+        print '.',
+    print    
+
+
+if __name__ == "__main__":   
+    
+    try:                  
+        compinfo()
+        starting()
+        ver_py()
+
+    except KeyboardInterrupt:
+        print '\nKeyboard INTERUPT (Ctrl+C)\nFIX ERROR AND TRY AGIN ! '
+    except:
+        print '\n\nERROR !!\nDISCONNECTED'
+        if platform.system() != 'Windows' :
+            print '\nNot tested on : ', platform.system()
+        print '\nPlease feedback: https://github.com/alosh55/STORM-BOT'
+        while True:
+            pass
